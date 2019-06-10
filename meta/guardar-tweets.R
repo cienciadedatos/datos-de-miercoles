b8f202d6f4b32a842e826ada6e9f611309840e74
Mauricio Vargas <mvargas@dcc.uchile.cl>
2019-06-10 19:40:36 -0400

    tweets semana 8

--- meta/guardar-tweets.R
+++ meta/guardar-tweets.R
@@ -3,9 +3,9 @@ pacman::p_load(rtweet, httpuv, dplyr, lubridate)
 
 data <- search_tweets("#datosdemiercoles", n = 18000, retryonratelimit = TRUE, include_rts = TRUE)
 
-semana6 <- data %>% 
+semana8 <- data %>% 
   filter(
-    created_at >= ymd_hms("2019-05-09 15:18:10") & created_at <= ymd_hms("2019-05-16 15:18:10")
+    created_at >= ymd_hms("2019-05-30 15:18:10") & created_at <= ymd_hms("2019-06-06 15:18:10")
   )
 
-save(semana6, file = glue::glue("meta/datos-de-miercoles-{ Sys.Date() }.rda"), compress = "xz")
+save(semana8, file = glue::glue("meta/datos-de-miercoles-{ Sys.Date() }.rda"), compress = "xz")
