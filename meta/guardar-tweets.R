b372dd8679e2c3ce04c346d80527e60c2bb32b6d
pachamaltese <mvargas@dcc.uchile.cl>
2019-08-27 11:20:54 -0400

    recupera semanas perdidad del disco

--- meta/guardar-tweets.R
+++ meta/guardar-tweets.R
@@ -1,4 +1,16 @@
 if (!require("pacman")) { install.packages("pacman") }
-pacman::p_load(rtweet, httpuv, dplyr, lubridate)
+pacman::p_load(rtweet, httpuv, dplyr, lubridate, glue)
 
-data <- search_tweets("#datosdemiercoles", n = 18000, retryonratelimit = TRUE, include_rts = TRUE)
+data <- search_tweets("#datosdemiercoles", n = 1000, type = "recent", retryonratelimit = TRUE, include_rts = TRUE)
+
+assign(glue("semana", week(Sys.Date() - days(1))),
+       data %>% 
+         filter(created_at > Sys.Date() - days(8) & 
+                  created_at < Sys.Date()
+         )
+)
+
+try(dir.create("meta"))
+try(dir.create(glue("meta/{year(Sys.Date())}")))
+
+save(semana34, file = glue("meta/{year(Sys.Date())}/{Sys.Date()}.rda"), compress = "xz")
